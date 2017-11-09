93afe60ac6364be41cd7ba77737e6de114adea98
Gab Santiago <gabbysantiago96@yahoo.com>
2017-11-10 02:05:49 +0800

    Query7 fully working

--- src/Model/Book.java
+++ src/Model/Book.java
@@ -4,7 +4,11 @@ public class Book {
 	private int bookID;
 	private String title;
 	private String publisherName;
+	private int borrowCount;
+	private String authorFirstName;
+	private String authorLastName;
 	private String authorFullName;
+	private String publisherAddress;
 	
 	public Book(int bookID, String title, String publisherName) {
 		super();
@@ -19,6 +23,25 @@ public class Book {
 		this.title = title;
 		this.publisherName = publisherName;
 		this.authorFullName = authorFullName;
+	}
+	
+	public Book(int bookID, String title, String publisherName, int borrowCount, String authorFirstName,
+			String authorLastName, String authorFullName) {
+		super();
+		this.bookID = bookID;
+		this.title = title;
+		this.publisherName = publisherName;
+		this.borrowCount = borrowCount;
+		this.authorFirstName = authorFirstName;
+		this.authorLastName = authorLastName;
+		this.authorFullName = authorFullName;
+	}
+	
+	public Book(String title, String authorFullName, String publisherName, String publisherAddress) {
+		this.title = title;
+		this.authorFullName = authorFullName;
+		this.publisherName = publisherName;
+		this.publisherAddress = publisherAddress;
 	}
 
 	public int getBookID() {
@@ -51,6 +74,38 @@ public class Book {
 
 	public void setAuthorFullName(String authorFullName) {
 		this.authorFullName = authorFullName;
+	}
+
+	public int getBorrowCount() {
+		return borrowCount;
+	}
+
+	public void setBorrowCount(int borrowCount) {
+		this.borrowCount = borrowCount;
+	}
+
+	public String getAuthorFirstName() {
+		return authorFirstName;
+	}
+
+	public void setAuthorFirstName(String authorFirstName) {
+		this.authorFirstName = authorFirstName;
+	}
+
+	public String getAuthorLastName() {
+		return authorLastName;
+	}
+
+	public void setAuthorLastName(String authorLastName) {
+		this.authorLastName = authorLastName;
+	}
+
+	public String getPublisherAddress() {
+		return publisherAddress;
+	}
+
+	public void setPublisherAddress(String publisherAddress) {
+		this.publisherAddress = publisherAddress;
 	}
 	
 	
